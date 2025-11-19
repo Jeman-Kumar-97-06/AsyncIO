09e19ca54dbd49106f4e9508885dca6b2ecc2e47
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-11-19 15:27:08 +0530

    Task Group/ Gather Coroutines and Tasks

--- Example7.py
+++ Example7.py
@@ -6,20 +6,24 @@ async def fetch_data(param):
     return f"Result of {param}"
 
 async def main():
+    #Creating tasks manually and awaiting one at a time:
     task1 = asyncio.create_task(fetch_data(1))
     task2 = asyncio.create_task(fetch_data(2))
     result1 = await task1
     result2 = await task2
     print(f"Task 1 and 2 awaited results : {[result1, result2]}")
 
+    # Gather Coroutines
     coroutines = [fetch_data(i) for i in range(1,3)]
     results    = await asyncio.gather(*coroutines,return_exceptions=True)
     print(f"Coroutines Results : {results}")
 
+    # Gather Tasks 
     tasks      = [asyncio.create_task(fetch_data(i)) for i in range(1,3)]
     results    = await asyncio.gather(*tasks)
     print(f"Task Results : {results}")
 
+    # Task Group :      
     async with asyncio.TaskGroup() as tg:
         results = [tg.create_task(fetch_data(i)) for i in range(1,3)]
     
